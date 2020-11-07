5d5269e0ff262b4d87ba98191eaebebae7f314f4
LukasMichalowski <lukpio97@gmail.com>
2020-11-07 21:15:02 +0100

    test

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -5,7 +5,48 @@
 class ResourceManager
 {
 public:
-ResourceManager() {};
+ResourceManager(Resource obiekt){
+    w = new obiekt;
+}
+
+~ResourceManager(){
+    cout<<"The cleaning service!!"<<endl;
+    delete w;
+}
+
+double get() { return k.get(); } // metoda double get()
+
+ResourceManager(const ResourceManager& a): w{a.w}, { } // konstruktor kopiujacy
+
+ResourceManager& operator=(const ResourceManager& t) // operator przypisania
+{
+        delete w;
+        w = t.w;
+        return *this;
+}
+
+ResourceManager(ResourceManager && a) // Move constructor
+{
+        w = a.w;
+        a.w = nullptr;
+        a.dlugosc = 0;
+        a.pojemnosc = 0;
+}
+
+ResourceManager& operator=(ResourceManager&& other) // move operator
+    {
+        if (this != &other)
+        {
+            cout<<"You try to assign the object to itself"<<endl;
+        }
+        delete w;
+        w= other.w;
+        other.w = nullptr;
+        return *this;
+    }
+
+
+
 private:
-Resource k;
+Resource* w;
 };
